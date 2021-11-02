426a257b4a8fba0a8bd0a262819d410bc048859a
AlifArshad04 <arshadbakshi@iut-dhaka.edu>
2021-11-02 13:59:02 +0600

    text input class added

---
+++ src/include/text_input.hpp
@@ -0,0 +1,117 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <sstream>
+#include <string>
+
+#define DELETE_KEY 8
+#define ENTER_KEY 13
+#define ESCAPE_KEY 27
+
+class Textbox
+{
+private:
+  sf::RectangleShape bg;
+  sf::Text textbox;
+  std::ostringstream text;
+  bool isSelected = false;
+  int limit;
+
+  void typeText(int ch)
+  {
+    if (ch == DELETE_KEY)
+    {
+      if (text.str().length() > 0)
+      {
+        backspace();
+      }
+    }
+    //else if (ch == ENTER_KEY)
+    //{
+    //	//selected(false);
+    //}
+    //else if (ch == ESCAPE_KEY)
+    //{
+    //}
+    else if (ch < 123)
+    {
+      text << (char)ch;
+    }
+    textbox.setString(text.str() + '_');
+  }
+
+  void backspace()
+  {
+    std::string temp = text.str();
+    temp.pop_back();
+    text.str("");
+    text << temp;
+    textbox.setString(text.str() + '_');
+  }
+
+public:
+  Textbox(sf::Font &font, sf::Vector2f pos, int size, sf::Color color, int lim, bool sel)
+  {
+    textbox.setFont(font);
+    textbox.setPosition(pos);
+    textbox.setCharacterSize(size);
+    textbox.setFillColor(color);
+    isSelected = sel;
+    limit = lim;
+    if (sel)
+    {
+      textbox.setString("_");
+    }
+    else
+    {
+      textbox.setString("");
+    }
+  }
+  void selected(bool sel)
+  {
+    isSelected = sel;
+    if (sel)
+    {
+      textbox.setString(text.str() + '_');
+    }
+    else
+    {
+      textbox.setString(text.str());
+    }
+  }
+
+  std::string getText()
+  {
+    return text.str();
+  }
+
+  void setText(std::string txt)
+  {
+    text.str(txt);
+    textbox.setString(text.str());
+  }
+
+  void drawTo(sf::RenderWindow &window)
+  {
+    window.draw(textbox);
+  }
+
+  void typedOn(sf::Event input)
+  {
+    if (isSelected)
+    {
+      int ch = input.text.unicode;
+      if (ch < 123)
+      {
+        if (text.str().length() < (size_t)limit)
+        {
+          typeText(ch);
+        }
+        else if (text.str().length() >= 4 && ch == DELETE_KEY)
+        {
+          backspace();
+        }
+      }
+    }
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,44 @@
 #include "include/grid.hpp"
 #include "include/node.hpp"
 #include "include/menu.hpp"
+#include "include/text_input.hpp"
 double GameWidth = 500;
 double GameHeight = 700;
 
 using namespace std;
 state currentState;
 
-int main(){
+int main()
+{
     srand(time(NULL));
-    sf::RenderWindow window(sf::VideoMode(GameWidth,GameHeight), "A Way Out", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(GameWidth, GameHeight), "A Way Out", sf::Style::Close);
     currentState = inMenu;
 
     int N = 8;
     int M = 5;
 
-    MainMenu menu(&window,"A Way Out",150,sf::Vector2f(GameWidth/2.0,GameHeight-150),15);
-    Level_Select_Menu level(&window,"Select Type",150,sf::Vector2f(GameWidth/2.0,GameHeight-150),15);
+    MainMenu menu(&window, "A Way Out", 150, sf::Vector2f(GameWidth / 2.0, GameHeight - 150), 15);
+    Level_Select_Menu level(&window, "Select Type", 150, sf::Vector2f(GameWidth / 2.0, GameHeight - 150), 15);
 
     Grid grid(N, M, &window);
     Node::setGameGrid(&grid);
-//     sf::Vector2f gridOffset(50,50);
-// 
-//     grid.setBoundary(gridOffset, sf::Vector2f(window.getSize())-gridOffset);
-//     grid.setBoxColor(sf::Color(94,83,83));
-//     grid.setBoundaryColor(sf::Color(94,83,83));
+    //     sf::Vector2f gridOffset(50,50);
+    //
+    //     grid.setBoundary(gridOffset, sf::Vector2f(window.getSize())-gridOffset);
+    //     grid.setBoxColor(sf::Color(94,83,83));
+    //     grid.setBoundaryColor(sf::Color(94,83,83));
 
-//     grid.show(); // numbers
+    //     grid.show(); // numbers
     grid.adjustNodes();
 
     sf::Image icon;
     icon.loadFromFile("Assets/icon.png"); // File/Image/Pixel
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
+    ////////////
+    sf::Font ft;
+    ft.loadFromFile("Assets/SourceCodePro-SemiBoldItalic.ttf");
+    Textbox tb(ft, Vector2f(0.0f, 30.0f), 22, sf::Color::White, 7, 1);
+    /////////////
     bool isWindowFocused = true;
 
     while (window.isOpen())
@@ -42,56 +48,83 @@ int main(){
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::GainedFocus) {
+            if (event.type == sf::Event::GainedFocus)
+            {
                 isWindowFocused = true;
             }
-            if(event.type == sf::Event::LostFocus) {
+            if (event.type == sf::Event::LostFocus)
+            {
                 isWindowFocused = false;
             }
 
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
-            if(event.type == sf::Event::KeyPressed) {
-                if(event.key.code == sf::Keyboard::Q) {
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Q)
+                {
                     window.close();
                 }
+            }
+            if (event.type == sf::Event::TextEntered)
+            {
+                //////////
+                bool fieldSelected = true;
+                if (fieldSelected)
+                {
+                    tb.typedOn(event);
+                }
+                /////////
             }
         }
 
         window.clear();
 
-        if(currentState == endProgram){
+        if (currentState == endProgram)
+        {
             break;
         }
 
-        if(currentState == inMenu) {
-            if(isWindowFocused) {
+        if (currentState == inMenu)
+        {
+            if (isWindowFocused)
+            {
                 menu.action();
             }
             menu.draw();
         }
 
-        else if(currentState == inLevelSelect){
-            if(isWindowFocused){
+        else if (currentState == inLevelSelect)
+        {
+            if (isWindowFocused)
+            {
                 level.action();
             }
             level.draw();
         }
-        
-        else if(currentState == inPlay) {
-            if(isWindowFocused) {
+
+        else if (currentState == inPlay)
+        {
+            if (isWindowFocused)
+            {
                 grid.takeInput();
             }
 
             grid.draw();
 
-            if(grid.isGameSolved()){
-                std::cout<<"Done"<<"\n";
+            if (grid.isGameSolved())
+            {
+                std::cout << "Done"
+                          << "\n";
                 return 0;
             }
         }
+        /////////
 
+        tb.drawTo(window);
+        /////////
         window.display();
     }
 
